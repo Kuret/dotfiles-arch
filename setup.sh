4aa4eba31cd5b4dbde5136b3750afdceb385ef33
Kuret <r.littel@outlook.com>
2018-06-19 12:39:13 +0200

    add autorandr to dots

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo rm -rf ~/.npmrc
 sudo rm -rf ~/.tmux.conf
 sudo rm -rf ~/.zshrc
 sudo rm -rf ~/.zsh
+sudo rm -rf ~/.config/autorandr
 sudo rm -rf ~/.config/nvim/init.vim
 sudo rm -rf ~/.config/nvim/config
 sudo rm -rf ~/.config/nvim/autoload
@@ -19,6 +20,9 @@ mkdir -p ~/.zsh
 ln -sf $PWD/dotfiles/zsh/alias.zsh ~/.zsh/alias.zsh
 ln -sf $PWD/dotfiles/zsh/plug.zsh ~/.zsh/plug.zsh
 
+mkdir -p ~/.config
+ln -sf $PWD/dotfiles/autorandr ~/.config
+
 mkdir -p ~/.config/nvim
 ln -sf $PWD/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf $PWD/dotfiles/nvim/config ~/.config/nvim
@@ -26,4 +30,3 @@ ln -sf $PWD/dotfiles/nvim/autoload ~/.config/nvim
 
 nvim --headless +UpdateRemotePlugins +qall >/dev/null 2>&1
 nvim --headless +PlugInstall +qall >/dev/null 2>&1
-
