a6cb3a3ec67fc33b1b264e32fab40d8a1c29150c
Kuret <r.littel@outlook.com>
2018-09-25 15:26:21 +0200

    add alacritty and twmn to dotfiles

--- setup.sh
+++ setup.sh
@@ -2,16 +2,25 @@
 set -e
 
 # Remove old links
+sudo rm -rf ~/.config/alacritty/alacritty.yml
 sudo rm -rf ~/.config/autorandr
+sudo rm -rf ~/.config/bspwm
 sudo rm -rf ~/.config/kak/kakrc
 sudo rm -rf ~/.config/nvim/init.vim
 sudo rm -rf ~/.config/nvim/config
 sudo rm -rf ~/.config/nvim/autoload
 sudo rm -rf ~/.config/ranger/rc.conf
 sudo rm -rf ~/.config/ranger/scope.sh
+sudo rm -rf ~/.config/sxhkd
+sudo rm -rf ~/.config/twmn/twmn.conf
 
 mkdir -p ~/.config
 ln -sf $PWD/dotfiles/autorandr ~/.config
+ln -sf $PWD/dotfiles/bspwm ~/.config
+ln -sf $PWD/dotfiles/sxhkd ~/.config
+
+mkdir -p ~/.config/alacritty
+ln -sf $PWD/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 mkdir -p ~/.config/kak
 ln -sf $PWD/dotfiles/kak/kakrc ~/.config/kak/kakrc
@@ -24,6 +33,9 @@ ln -sf $PWD/dotfiles/nvim/autoload ~/.config/nvim
 mkdir -p ~/.config/ranger
 ln -sf $PWD/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
 ln -sf $PWD/dotfiles/ranger/scope.sh ~/.config/ranger/scope.sh
+
+mkdir -p ~/.config/twmn
+ln -sf $PWD/dotfiles/twmn/twmn.conf ~/.config/twmn/twmn.conf
 
 nvim --headless +UpdateRemotePlugins +qall >/dev/null 2>&1
 nvim --headless +PlugInstall +qall >/dev/null 2>&1
